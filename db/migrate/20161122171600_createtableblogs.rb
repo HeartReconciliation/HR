664bd324a5f8e7b2ea96bdd7c4076d51ab3c7437
gotens1211 <nikeythe@gmail.com>
2016-11-22 22:49:05 +0530

    blogs table issue

---
+++ db/migrate/20161122171600_createtableblogs.rb
@@ -0,0 +1,10 @@
+class Createtableblogs < ActiveRecord::Migration
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
